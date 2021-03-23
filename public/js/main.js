6b6f95c1949bcb8847010af479040e1d48fc1c6c
stallion7 <stepankrautwurst@gmail.com>
2021-03-23 23:35:54 +0100

    upravy
    úprava chat.html a zbylých drobností

--- public/js/main.js
+++ public/js/main.js
@@ -38,7 +38,7 @@ chatForm.addEventListener('submit', (e) => {
 function outputMessage(message){
     const div = document.createElement('div');
     div.classList.add('message');
-    div.innerHTML = ` <p class="meta">${message.username} <span>${message.time}</span></p>
+    div.innerHTML = ` <p class="meta"><b>${message.username}</b> <span>${message.time}</span></p>
     <p class="text">
      ${message.text}
     </p>`;
